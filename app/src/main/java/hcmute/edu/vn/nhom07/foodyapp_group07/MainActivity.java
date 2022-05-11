18440bb8b08f20ac535bfa7595a702637c75aeea
khanhngan501 <khanhngan501@gmail.com>
2022-05-11 21:34:03 +0700

    Sign Up Commit

--- app/src/main/java/hcmute/edu/vn/nhom07/foodyapp_group07/MainActivity.java
+++ app/src/main/java/hcmute/edu/vn/nhom07/foodyapp_group07/MainActivity.java
@@ -3,12 +3,31 @@ package hcmute.edu.vn.nhom07.foodyapp_group07;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button signIn_btn, skip_btn;
+    TextView txt_signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signIn_btn = (Button) findViewById(R.id.signIn_btn);
+        skip_btn = (Button) findViewById(R.id.skip_btn);
+        txt_signUp = (TextView) findViewById(R.id.txt_signUp);
+
+        txt_signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
     }
 }
